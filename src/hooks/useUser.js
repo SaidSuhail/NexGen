5d9c43fcdbf4988268c2006f244cad436360bc25
Saidsuhailvp <suhailsaid999@gmail.com>
2026-03-11 22:29:10 +0530

    okk

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { getUsers } from "../Api/userapi";
+import { getUsers } from "../Api/userApi";
 
 export const useUsers = () => {
   return useQuery({
