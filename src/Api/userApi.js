7037e054fa607602479f49c5b33b9605c75edcf2
Saidsuhailvp <suhailsaid999@gmail.com>
2026-03-12 22:55:43 +0530

    Api Name Change

--- src/Api/userApi.js
+++ src/Api/userApi.js
@@ -1,6 +1,6 @@
 import api from "./axios";
 
 export const getUsers = async () => {
-  const response = await api.get("/COM_API_/CMUser/GetUsers");
+  const response = await api.get("/COM_API_/CMUser/GetAllUsers");
   return response.data;
 };
